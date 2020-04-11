21cc4558e3a458e02ff09423aaaec85ad0dd2508
Kirill Enykeev <enykeev@mech.sh>
2020-04-12 00:08:18 +0700

    ploish api component

--- src/components/api.js
+++ src/components/api.js
@@ -5,49 +5,52 @@ const express = require('express')
 const pubsub = require('../pubsub')
 const models = require('../models')
 
-// Server
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+router.get('/executions', async (req, res) => {
+  const collection = await models.Executions
+    .fetchAll({
+      withRelated: ['result']
+    })
+    .map(execution => {
+      return {
+        ...execution.serialize(),
+        result: execution.relations.result.get('result')
+      }
+    })
+
+  res.json(collection)
+})
+
+async function handleMessages (msg) {
+  const message = JSON.parse(msg.content.toString())
+  console.log('%s:%s', msg.fields.routingKey, util.inspect(message))
+
+  if (msg.fields.routingKey === 'execution') {
+    const mod = models.Executions.forge(message)
+    await mod.save(null, { method: 'insert' })
+  }
+
+  if (msg.fields.routingKey === 'result') {
+    const mod = models.Results.forge(message)
+    await mod.save(null, { method: 'insert' })
+  }
+
+  pubsub.channel.ack(msg)
+}
+
 async function main () {
   await pubsub.init()
-
-  pubsub.subscribe('*', async msg => {
-    const message = JSON.parse(msg.content.toString())
-    console.log('%s:%s', msg.fields.routingKey, util.inspect(message))
-
-    if (msg.fields.routingKey === 'execution') {
-      const mod = models.Executions.forge(message)
-      await mod.save(null, { method: 'insert' })
-    }
-
-    if (msg.fields.routingKey === 'result') {
-      const mod = models.Results.forge(message)
-      await mod.save(null, { method: 'insert' })
-    }
-
-    pubsub.channel.ack(msg)
-  })
+  pubsub.subscribe('*', handleMessages)
 
   const app = express()
-
   app.use(express.json())
 
-  app.get('/', (req, res) => {
-    res.send('Hello World')
-  })
-
-  app.get('/executions', async (req, res) => {
-    const collection = await models.Executions
-      .fetchAll({
-        withRelated: ['result']
-      })
-      .map(execution => {
-        return {
-          ...execution.serialize(),
-          result: execution.relations.result.get('result')
-        }
-      })
-
-    res.json(collection)
-  })
+  app.use(router)
 
   app.listen(3000)
 }
