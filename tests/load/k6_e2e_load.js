546bb1a3a1cb17fdfcd56188a94b5233e8578838
Kirill Enykeev <enykeev@mech.sh>
2020-05-05 19:51:46 +0700

    setup grafana and rabbitmq-prometheus

---
+++ tests/load/k6_e2e_load.js
@@ -0,0 +1,22 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+  stages: [
+    { duration: '5m', target: 100 },
+    { duration: '10m', target: 100 },
+    { duration: '5m', target: 0 }
+  ]
+}
+
+export default () => {
+  http.post('http://sensor:3001/', JSON.stringify({
+    type: 'e2e',
+    url: 'http://sink'
+  }), {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  sleep(1)
+}
